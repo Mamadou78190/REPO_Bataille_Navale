ab24a611b6984b7491fc59f4d9db5dc6fe42fdba
Mamadou78190 <115724144+Mamadou78190@users.noreply.github.com>
2022-11-01 14:54:35 +0100

    Ajout de fichier dans branche
    App.java : Main pour tester les classes
    grille.java : crée la grille et place des bateaux aléatoirement sans collision
    personnage.java : représentation de teste des bateaux

---
+++ modele/personnage.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class personnage {
+
+    int taille = 5;
+    String caract;
+
+    public personnage(String caract){
+       this.caract = caract;
+    }
+   
+    
+    
+}
