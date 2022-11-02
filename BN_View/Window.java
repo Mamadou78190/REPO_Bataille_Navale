932b5a99113094971cec4b1d96cc08352669b54c
Azlaf Abdoul Vahab <94910590+frappedenewdelhi@users.noreply.github.com>
2022-11-02 19:58:20 +0100

    Window.java : Creating a new blank window

---
+++ BN_View/Window.java
@@ -0,0 +1,16 @@
+package BN_View;
+import javax.swing.*;
+
+public class Window extends JFrame {
+    
+    public Window()
+    {
+        super("WarShip game of Aziz");
+    }
+    public static void main(String[] args) 
+    {
+        Window window = new Window();
+        window.setVisible(true);
+        
+    }
+}
