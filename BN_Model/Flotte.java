e624269d8776c67d0663a52b9d237e6c1cbeb6e4
MahamadouN <mamadou.n78190@gmail.com>
2022-11-09 19:17:38 +0100

    Flotte : Changement de la création d'une flotte

--- BN_Model/Flotte.java
+++ BN_Model/Flotte.java
@@ -9,15 +9,15 @@ public class Flotte {
     {
 
         for (int i=0;i<4;i++){//creation de 4 sous-marin
-            flotte[i] = new Ship(0,0,caracteristiquesBateaux.sousMarin);
+            flotte[i] = new Ship(caracteristiquesBateaux.sousMarin);
         }
 
         for (int i=4;i<7;i++){//creation de 3 destroyer
-            flotte[i] = new Ship(0,0,caracteristiquesBateaux.destroyer);
+            flotte[i] = new Ship(caracteristiquesBateaux.destroyer);
         }
 
         for (int i=7;i<9;i++){//creation de 2 croiseur
-            flotte[i] = new Ship(0,0,caracteristiquesBateaux.croiseur);
+            flotte[i] = new Ship(caracteristiquesBateaux.croiseur);
         }
 
     }
