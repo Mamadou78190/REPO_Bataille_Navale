aeac5d587ec75896b08bf0f69c499cdbe4ad13a0
MahamadouN <mamadou.n78190@gmail.com>
2022-11-10 22:06:54 +0100

    DeplacementNavire : Gestion du Sous-marin (toute direction)

--- BN_Model/DeplacementNavire.java
+++ BN_Model/DeplacementNavire.java
@@ -7,9 +7,10 @@ public class DeplacementNavire {
       
         int position = n1.getOrientation();
 
+      if (n1.getTaille() > 1){
+
         switch (position){
             //verticale
-
             case 0 : 
 
                 switch (direction){
@@ -132,7 +133,54 @@ public class DeplacementNavire {
             break;
             
         }
-                
+      } else if (n1.getTaille() == 1){
+
+            switch (direction){
+
+                case "gauche" :
+            
+                if (ApparitionFlotte.balayageGauche(n1, n1.getY(),n1.getX()-1,g1) == 1){
+                    g1.cacherCaseGrille(n1.getX(), n1.getY());
+                    g1.grilleJeu[n1.getX()-1][n1.getY()] = n1.getVisuel();
+                    System.out.println("Sous-Marin déplacé à gauche");
+                    g1.afficherGrille();
+                }else{System.out.println("Impossibilité d'aller à gauche");}
+
+                break;
+
+
+                case "droite" :
+
+                if(ApparitionFlotte.balayageDroite(n1, n1.getY(),n1.getX()+1,g1) == 1){
+                    g1.cacherCaseGrille(n1.getX(), n1.getY());
+                    g1.grilleJeu[n1.getX()+1][n1.getY()] = n1.getVisuel();
+                    System.out.println("Sous-Marin déplacé à droite");
+                    g1.afficherGrille();
+                }else{System.out.println("Impossibilité d'aller à droite");}
+
+                break;
+
+                case "haut" : 
+
+                if(ApparitionFlotte.balayageHaut(n1, n1.getY()-1,n1.getX(),g1) == 1){
+                    g1.cacherCaseGrille(n1.getX(), n1.getY());
+                    g1.grilleJeu[n1.getX()][n1.getY()-1] = n1.getVisuel();
+                    System.out.println("Sous-Marin déplacé en haut");
+                    g1.afficherGrille();
+                }else{System.out.println("Impossibilité d'aller plus haut");}
+
+                break;
+
+                case "bas" :
+
+                if(ApparitionFlotte.balayageBas(n1, n1.getY()+1,n1.getX(),g1) ==  1){
+                    g1.cacherCaseGrille(n1.getX(), n1.getY());
+                    g1.grilleJeu[n1.getX()][n1.getY()+1] = n1.getVisuel();
+                    System.out.println("Sous-Marin déplacé en bas");
+                    g1.afficherGrille();
+                }else{System.out.println("Impossibilité d'aller plus bas");}
+            }    
+        }          
     }
 
 }
